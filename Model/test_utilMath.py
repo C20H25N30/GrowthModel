04efbd2540667f6a8f061df291a7915a2cd11306
Cordes-RJ <richardj.cordes@gmail.com>
2020-03-18 22:18:58 -0400

    BUGFIX:1 Rng given percentile fixed and tested

---
+++ Model/test_utilMath.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+import matplotlib.pyplot as plt
+import numpy as np
+import utilMath
+
+#Testing RandGivenPercentile
+def getWalkParams(xT,xN):
+    return np.average([xT,xN]),np.std([xT,xN])
+
+# Generator Object
+class testWalk:
+    def __init__(self,count,xT,xN, **kwargs):
+        self.xT = xT
+        self.xN = xN
+        self.MaxCt = count
+        self.Count = 0
+        self.kwargs = kwargs
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.Count >= self.MaxCt:
+            raise StopIteration
+        self.Count += 1
+        return utilMath.RandGivenPercentile(self.xT,self.xN,**self.kwargs)
+
+#parameters
+xT = 0.4
+xN = 0.9
+mu,sigma = np.mean([xN,xT]),np.std(([xN,xT]))
+x = testWalk(100000,xN,xT,Max=1, Min = 0, recursionLimit = 3)
+
+#run generator
+Array = []
+for i in x:
+    Array.append(i)
+
+# print stats-lite
+mu2,sigma2 = np.mean(Array),np.std(Array)
+print("Given Mu, Sigma: ",mu, sigma)
+print("Found Mu, Sigma: ", mu2, sigma2)
+print("Given 10th: ", xT)
+print("Found 10th: ", np.percentile(Array,10))
+print("Given 90th: ", xN)
+print("Found 90th: ", np.percentile(Array,90))
+
+# print chart
+count, bins, ignored = plt.hist(Array, 30, density=True)
+plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) *
+               np.exp( - (bins - mu)**2 / (2 * sigma**2) ),
+         linewidth=2, color='r')
+
+plt.xlim(0,1)
+plt.show()
+
+#%%
+
--- Model/utilMath.py
+++ Model/utilMath.py
@@ -6,20 +6,32 @@ utilMath holds misc. math functions and constants
 
 import numpy as np
 
-walkStdev = 0.779 # walkStdDev is the value of standard deviation at which
-                  # numpy's random.normal distribution will have roughly
-                  # equal 10th and 90th percentiles to those given. 
+# RandGivenPercentile will output a random number from a theoretical set
+# with 10th and 90th percentiles which approach those given as arguments. 
+def RandGivenPercentile(xT,xN, **kwargs):
+    # find est. mean and stdev of theoretical distribution
+    mu, sigma = np.average([xT,xN]),np.std([xT,xN])
+    # check for recursion limit
+    if "recursionLimit" in kwargs.keys(): # then use recursion limit
+        recursionLimit = kwargs["recursionLimit"]
+    else: # set default
+        recursionLimit = 2
+    return randGivenPtilesGenAttempt(mu,sigma,0,recursionLimit,**kwargs)
 
-# Walk takes the est. 10th percentile and 90th percentile values of a theoretical dataset
-# and creates a random value within a normally distributed set that has matching
-# 10th and 90th percentile values.
-def Walk(x10th,x90th,**kwargs):
-    rng = np.random.normal(0, walkStdev)
-    x = rng * ((x90th-x10th)/2) +(x10th+x90th)/2
+# randGivenPtilesGenAttempt is the primary function within RandGivenPercentile
+# separated for the sake of removing redundancy in recursion.
+def randGivenPtilesGenAttempt(mu,sigma,depth,limit,**kwargs):
+    x = np.random.normal(mu,sigma)
     if "Max" in kwargs.keys():
-        if x > kwargs["Max"]:
-            return kwargs["Max"]
+        if x >= kwargs["Max"]: 
+            if depth <= limit:
+                return randGivenPtilesGenAttempt(mu,sigma,depth+1,limit,**kwargs)
+            else:
+                return kwargs["Max"]
     if "Min" in kwargs.keys():
-        if x < kwargs["Min"]:
-            return kwargs["Min"]
+        if x <= kwargs["Min"]: 
+            if depth <= limit:
+                return randGivenPtilesGenAttempt(mu,sigma,depth+1,limit,**kwargs)
+            else:
+                return kwargs["Min"]
     return x
